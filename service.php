fe06c4b47d3e64b5a2dbba3e38bbdc90a5eb02db
Daniel Sikar <dsikar@gmail.com>
2019-06-27 15:27:39 +0000

    Clearing up queue after request for service.

--- service.php
+++ service.php
@@ -1,22 +1,23 @@
 <?php
+        #studio-ini.php
+        #Get or set remote BNO055 sensor readings.
+        #This page services the write requests from sensors and read
+        #requests from PLC client.
 
-        //studio-ini.php
-        //Get or set remote BNO055 sensor readings.
-        //This page services the write requests from sensors and read
-        //requests from PLC client.
-
-
-// Parse action, sensor and payload as required
+# Parse action, sensor and payload as required
 
 $action = (isset($_GET['action']) ? $_GET['action'] : null);
-if ($action == "r") { //if action is equal to read
+if ($action == "r") { #if action is equal to read
         $cmd = "tail -1 queue.txt";
         $cmdout = shell_exec($cmd);
         echo $cmdout;
-} elseif ($action == "w") { //if action is equal to write
+        $emptyStr = "";
+	$deleteQueueContent = "echo \"$emptyStr\" > queue.txt";
+        shell_exec($deleteQueueContent);
+} elseif ($action == "w") { #if action is equal to write
         $payload = (isset($_GET['payload']) ? $_GET['payload'] : null);
         $cmd = "echo \"$payload\" > serviced.txt";
-        // echo $cmd;
+        echo $cmd;
         shell_exec($cmd);
 } else {
         echo "Malformed URL - action=(w|r) required.";
