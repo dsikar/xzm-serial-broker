10c5b61f4e0c17016998289062a9e974a6a9cc56
Daniel Sikar <dsikar@gmail.com>
2019-07-10 15:00:46 +0000

    Updated service.php

--- service.php
+++ service.php
@@ -9,16 +9,11 @@ if ($action == "r") { #if action is equal to read
         $emptyStr = "";
 	$deleteQueueContent = "echo \"$emptyStr\" > queue.txt";
         shell_exec($deleteQueueContent);
-	// log empty quey
-	$str = "echo \"[2] - Job request removed from queue.\" >> log.txt" . "\n" . "\n";
-	shell_exec($str);
 } elseif ($action == "w") { #if action is equal to write
         $payload = (isset($_GET['payload']) ? $_GET['payload'] : null);
         $cmd = "echo \"$payload\" > serviced.txt";
         echo $cmd;
         shell_exec($cmd);
-	$str = "echo \"[3] - Job completed.\" >> log.txt" . "\n" . "\n";
-	shell_exec($str);
 } else {
         echo "Malformed URL - action=(w|r) required.";
 }
