db6ee819cdac466e9c4347b66bf9e38ec0903003
Daniel Sikar <dsikar@gmail.com>
2019-09-26 14:44:59 +0000

    Deleted files.

--- filesaving.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-#Receive request from android phone for file saving
-$action = $_GET['action'];
-print $action;
-$cmd = "echo \"$action\" > filesaving.txt";
-$cmdout = shell_exec($cmd);
-
-?>
